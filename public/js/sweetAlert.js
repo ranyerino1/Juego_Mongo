6909e1aa8ef6fc2a739b2976a12fbd9b2618dd63
ranyerino1 <jetacuma3@misena.edu.co>
2022-12-07 01:12:24 -0500

    cambio

--- public/js/sweetAlert.js
+++
@@ -1,9 +0,0 @@
-
-Swal.fire({
-	title: "Hola",
-	 //text: '<b>1.</b> dar click en el botón de lanzar moneda\n2. luego de que la moneda gire vas a dar click en el botón de iniciar el juego',
-	 html: '<b>Te haremos una serie de preguntas de selección múltiple. Da click en el botón de ver evaluación y elige tus respuestas, buena suerte :D</b>',
-	icon: 'success'
-	
-});
-
