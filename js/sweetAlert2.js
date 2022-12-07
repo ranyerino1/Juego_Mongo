45ee387efa4d54e7d0fc4fbf1ab2a39a0ee18d26
milenaaristizabal <104436773+milenaaristizabal@users.noreply.github.com>
2022-12-06 22:55:20 -0500

    Estilos del juego

---
+++ js/sweetAlert2.js
@@ -0,0 +1,7 @@
+
+Swal.fire({
+	 //text: '<b>1.</b> dar click en el botón de lanzar moneda\n2. luego de que la moneda gire vas a dar click en el botón de iniciar el juego',
+	 html: '<b>Te mostraremos unas instrucciones las cuales te prepararán para la evaluación. Cuando te sientas preparado/a da click en realizar evaluación</b>',
+	icon: 'success'
+	
+});
